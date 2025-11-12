59f65f549fc20bf366dd59d1bdca9f6c42449c42
zfeixiang <zhangfeixiang9@h-partners.com>
2025-11-12 19:05:08 +0800

    Check for valid position
    Signed-off-by: zfeixiang <zhangfeixiang9@h-partners.com>

--- elfio/elfio_section.hpp
+++ elfio/elfio_section.hpp
@@ -217,7 +217,7 @@ template <class T> class section_impl : public section
     {
         if ( get_type() != SHT_NOBITS ) {
             // Check for valid position
-            if ( pos >= get_size() || size != get_size() - pos ) {
+            if ( pos >= get_size() || size > get_size() - pos ) {
                 return;
             }
             Elf_Xword new_size = get_size() - size;
